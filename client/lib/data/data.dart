0a447a47cba33992924d102644389687b9d260d4
moritz <moritz.schoppe@proton.me>
2022-10-16 19:23:23 +0200

    delete first and second page

--- client/lib/data/data.dart
+++ client/lib/data/data.dart
@@ -1,12 +1,8 @@
-import 'package:client/screens/first_page/first_page.dart';
 import 'package:client/screens/markdown_page/markdown_page.dart';
-import 'package:client/screens/second_page/second_page.dart';
 import 'package:flutter/cupertino.dart';
 
 class Data {
   final availablePages = <String, WidgetBuilder>{
-    'First Page': (_) => const FirstPage(),
-    'Second Page': (_) => const SecondPage(),
     'Markdown Page': (_) => const MarkdownPage(),
   };
 
